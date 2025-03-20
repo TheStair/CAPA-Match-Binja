e54f9744867537431fd1d35c9ddfb8e0dacc6d98
TheStair <thestairmn@proton.me>
2025-03-20 15:52:49 -0500

    Initial Progress

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# CAPA-Match
+# CAPA-Match-Binja
 Binary Ninja Plugin to Match Binaries to CAPA rules
+
+**About This Project**
+I took on this project with the goal of learning how to interface with a large pre-existing software project. I am totally new to the Binary Ninja API. As you can likely tell, this file will contain unused functions that I used to teach myself.
+
+**Personal Goals for this project**
+1. No Code from AI  
+&nbsp;&nbsp;&nbsp;&nbsp; With the recent explosion of the trend of "*vibe coding*". I really want to take the time to sharpen up my fundamental programming skills.
+
+2. Interface with a large pre-existing software project  
+&nbsp;&nbsp;&nbsp;&nbsp; Learning to properly read documentation for APIs is a key skill, and something I really want to develop for my future self.
+
+3. Increase my understanding of the Reverse-Engineering World  
+&nbsp;&nbsp;&nbsp;&nbsp; With a career interest in Reverse-Engineering, Digital Forensics, and Incident Response, Learning Reverse-Engineering concepts and developing skills with RE tools, I can best prepare for my future career.
--- __init__.py
+++ __init__.py
@@ -1,7 +1,38 @@
+#Author TheStair
+
+#Imports
 from binaryninja import *
+import sys
+from pathlib import Path
 
-def do_nothing(bv):
-	show_message_box("Do Nothing", "Congratulations! You have successfully done nothing.\n\n" +
-					 "Pat yourself on the back.", MessageBoxButtonSet.OKButtonSet, MessageBoxIcon.ErrorIcon)
+# def do_nothing(bv):
+#	show_message_box("Do Nothing", "Congratulations! You have successfully done nothing.\n\n" +
+#					 "Pat yourself on the back.", MessageBoxButtonSet.OKButtonSet, MessageBoxIcon.ErrorIcon)
 
-PluginCommand.register("Useless Plugin", "Basically does nothing", do_nothing)
+# PluginCommand.register("Useless Plugin", "Basically does nothing", do_nothing)
+
+# Prompt for user input and return the filepath
+def get_input_file():
+    input_file_path_str = input("Please Enter the File to be analyzed: ").strip().lower()
+    if input_file_path_str != "":
+        file_path = Path(input_file_path_str)
+    else:
+        print("Please Enter a Filepath")
+        sys.exit()
+    return file_path
+
+# Prints Funtion Names and Addresses (Mostly a Test function)
+def print_file_function_names():
+
+    functions = bv.functions
+    for func in functions:
+        print(f"Function {func.name} starts at 0x{func.start:x}")
+
+
+#Script Start
+#Loads user-specified file
+input_file = get_input_file()
+with load(input_file) as bv:
+    print("Analyzing File")
+
+print_file_function_names()
